eac14a8497d5c6fe56b5430c81e18a3834c25898
Elijah Atkins <elijahorama@gmail.com>
2020-10-06 21:03:59 -0600

    day1 mvp done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,11 +4,17 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
-
-    pass
+    #if there are less than zero cookies return 0
+    if (n < 0):
+        return 0
+    #return 1 to end recusion when 
+    elif (n==1 or n==0):
+        return 1
+    else:
+        return (eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3))
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 30
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
