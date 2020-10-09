a4252ac44006f61aff7815f34a5e7c575e3bf56f
Elijah Atkins <elijahorama@gmail.com>
2020-10-08 18:58:10 -0600

    day2 start

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,6 +2,7 @@
 Input: an integer
 Returns: an integer
 '''
+'''
 def eating_cookies(n):
     # Your code here
     #if there are less than zero cookies return 0
@@ -12,9 +13,20 @@ def eating_cookies(n):
         return 1
     else:
         return (eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3))
+'''
+def cookie_tracker(arr):
+    next_value = sum(arr)
+    return arr[1:] + [next_value]
 
+def eating_cookies(n, cache=None):
+    tracker = [0,0,1]
+    while n > 1:
+        tracker = cookie_tracker(tracker)
+        n -= 1
+    return sum(tracker)
+import sys
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 30
+    num_cookies = int(sys.argv[1])
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
